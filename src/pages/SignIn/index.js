0cdeb9204f4bc72725bcc78933a4242611f829c6
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-15 20:00:13 -0300

    Login and create account pages created

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/dog-svgrepo-com.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoDog" />
+      <form>
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Submit</button>
+        <Link to="register">Sign Up!</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/dog-svgrepo-com.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoDog" />
+      <form>
+        <input type="text" placeholder="Your name" />
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Create Account</button>
+        <Link to="/">Already have an account!</Link>
+      </form>
+    </>
+  );
 }
