b5b90dc9cb410fd57eef5aacd6b3a19d6b9204dc
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-21 11:27:29 -0300

    Update profile page implemented, including file upload and header data showing user data

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logo from '~/assets/dog-svgrepo-com.svg';
 import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -16,11 +19,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Bruno</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My Profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Bruno"
             />
           </Profile>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { Container } from './styles';
 
-// import { Container } from './styles';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Your full name" />
+        <Input name="email" placeholder="Your e-mail address" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm new password"
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+      <button type="button">Log Out</button>
+    </Container>
+  );
 }
